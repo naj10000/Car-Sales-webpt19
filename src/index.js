f76bae9705173815c25fe7992c9d5ad739f9f80f
NaJ <naj10000@live.com>
2020-09-22 23:56:34 -0400

    mvp

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux';
+import { Reducer } from './reducers/Reducer'
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(Reducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<Provider><App /></Provider>, rootElement);
+ReactDOM.render(<Provider store={store} ><App /></Provider>, rootElement);
